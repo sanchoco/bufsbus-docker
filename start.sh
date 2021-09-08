e89d7f35d650c9cbf9653f697f014623b0cb5a1c
sanchoco <jguuun@gmail.com>
2021-09-08 12:02:35 +0900

    Edit start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
-docker stop bufsbus-server bufsbus-mysql > /dev/null 2>&1 
-docker rm bufsbus-server bufsbus-mysql > /dev/null 2>&1
-docker-compose up -d --build
+docker-compose up --force-recreate -d --build
+docker image prune -f

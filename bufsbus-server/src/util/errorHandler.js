ea7813cb44f6840115a5dba0cd9acb5eb4b89f9d
sanchoco <jguuun@gmail.com>
2022-05-27 09:33:23 +0900

    Update time shuttle table

--- bufsbus-server/src/util/errorHandler.js
+++ bufsbus-server/src/util/errorHandler.js
@@ -1,4 +1,4 @@
 module.exports = (err, req, res, next) => {
-    console.log('error!!\n' + err.stack);
-    res.status(500).json({ error: err.stack });
+  console.log('error!!\n' + err.stack);
+  res.status(500).json({ error: err.stack });
 }
